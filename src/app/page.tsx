919d26a411c7d6c1b0f28310a7fa7ea5ac44f80e
permanaa <irmanpermana.lu@gmail.com>
2024-05-25 16:49:10 +0700

    feat: preview entry point

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,12 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24 gap-6">
       <Link href='/drag-n-drop'>
         Drag n Drop
+      </Link>
+      <Link href='/preview'>
+        Preview
       </Link>
     </main>
   );
